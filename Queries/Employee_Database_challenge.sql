6d8ac8df6491a780264138eb8d90cf9530e06594
Soniaprogram <soniashar95@gmail.com>
2021-02-12 21:22:31 -0500

    Deliverable 1

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,46 @@
+-- Deliverable 1
+-- Steps 1-7
+-- Retrieve the emp_no, first_name, and last_name columns from the Employees table.
+-- Retrieve the title, from_date, and to_date columns from the Titles table.
+-- Create a new table using the INTO clause.
+-- Join both tables on the primary key.
+SELECT employees.emp_no,
+	employees.first_name,
+	employees.last_name,
+	titles.title,
+	titles.from_date,
+	titles.to_date
+INTO retirement_titles
+FROM employees
+INNER JOIN titles
+ON employees.emp_no = titles.emp_no
+WHERE (employees.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY employees.emp_no;
+-- Filter the data on the birth_date column to retrieve the employees who were born
+-- between 1952 and 1955. Then, order by the employee number.
+SELECT * FROM retirement_titles
+
+-- Steps 8-14
+-- Retrieve the employee number, first and last name, and title columns from the 
+-- Retirement Titles table. Use Distinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+-- Sort the Unique Titles table in ascending order by the employee number and 
+-- descending order by the last date of the most recent title.
+SELECT * FROM unique_titles
+
+-- Steps 15-21
+-- First, retrieve the number of titles from the Unique Titles table.
+-- Then, create a Retiring Titles table to hold the required information.
+SELECT count(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count(title) DESC;
+-- Group the table by title, then sort the count column in descending order.
+SELECT * FROM retiring_titles
